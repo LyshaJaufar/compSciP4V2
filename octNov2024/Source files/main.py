795c3f235be16a32962cf6cdd95d6a77967f5977
LyshaJaufar <lysha.ahmd@gmail.com>
2025-04-27 07:47:33 +0500

    upd

--- octNov2024/Source files/main.py
+++ octNov2024/Source files/main.py
@@ -13,4 +13,41 @@ class EventItem:
     def getType(self):
         return self.Type
     
+class Character:
+    # Attribute declarations
+    def __init__(self, characName, jump, swim, run, drive):
+        self.characName = characName
+        self.jump = jump                      
+        self.swim = swim                     
+        self.run = run                    
+        self.drive = drive       
+
+        self.skills = {  
+            "jump": jump,
+            "swim": swim,
+            "run": run,
+            "drive": drive
+        }
+
+    def GetName(self):
+        return self.characName
+
+    def CalculateScore(self, eventType, difficulty):
+        successTable = {1: 80, 2: 60, 3: 40, 4: 20}
+
+        skillLevel = self.skills.get(eventType, None)
+
 Group = []
+
+# Data for the events
+events = [
+    ("Bridge", "jump", 3),
+    ("Water wade", "swim", 4),
+    ("100 mile run", "run", 5),
+    ("Gridlock", "drive", 2),
+    ("Wall on wall", "jump", 4)
+]
+
+for name, event_type, difficulty in events:
+    Group.append(EventItem(name, event_type, difficulty))
+
